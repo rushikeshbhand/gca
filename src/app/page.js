1c0b07c03e73bfe9fbed5100baa97e1c755d1314
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-09-24 08:37:05 +0530

    create success rate section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import Hero from "./components/hero/page"
-import Achievements from "./components/achievements/page"
-import About from "./components/about/page"
+import Hero from "./components/hero/page";
+import Achievements from "./components/achievements/page";
+import About from "./components/about/page";
+import SuccessRate from "./components/success-rate/page";
 export default function Home() {
   return (
     <div>
       <Hero />
-      <Achievements/>
+      <Achievements />
       <About />
+      <SuccessRate />
     </div>
   );
 }
