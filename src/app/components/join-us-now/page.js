1f634cc395fdf27b2c829ab90dd44944c9a9a936
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-09-28 23:27:00 +0530

    create join us now component but third card of exam portal is not responsive for only mobile view it is responsive for views larger than sm

---
+++ src/app/components/join-us-now/page.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function JoinUsNow() {
+  return (
+    <div className="container mb-80">
+      <div className="text-center text-2xl md:text-3xl lg:text-5xl text-[#4F4D74]">
+        <h1>Join Us Now!</h1>
+      </div>
+
+      <div className="cards text-white flex flex-col justify-center gap-8">
+        <div className="card-one flex flex-col lg:flex-row items-center gap-10 lg:gap-16 mt-24 max-w-[100vw] bg-[#6072FA] border-8 border-[#f9f9fa] rounded-3xl drop-shadow-2xl">
+          <div className="relative bottom-20 -mb-20">
+            <img
+              src="assets/images/tiger-with-boy.svg"
+              alt="tiger with boy"
+              className="w-[80%] h-[auto]"
+            />
+          </div>
+
+          <div className="flex flex-col gap-4 pb-10">
+            <h1 className="text-4xl font-extrabold">Book a Franchise</h1>
+            <p>
+              Begin your teaching venture now, own your <br /> franchise and
+              earn right from your doorstep.
+            </p>
+            <button className="px-10 py-3 bg-white text-[#6072FA] rounded-3xl border-4 border-[#e3e3e6] w-[180px]">
+              Book Now
+            </button>
+          </div>
+        </div>
+
+        <div className="card-two-three-root flex flex-col lg:flex-row gap-10">
+          <div className="card-two flex flex-col h-[55vh] lg:w-1/3 gap-14 bg-[#74D997] py-16 px-24 border-8 border-[#f9f9fa] rounded-3xl drop-shadow-2xl">
+            <div className="text-white text-4xl font-extrabold whitespace-nowrap">
+              <h1>
+                Book Demo <br /> Class Now
+              </h1>
+            </div>
+            <button className="px-10 py-3 text-white bg-[#6072FA] rounded-3xl border-4 border-white w-[180px]">
+              Book Now
+            </button>
+          </div>
+
+          <div className="card-three bg-[#17C7EE] h-[55vh] lg:w-2/3 border-8 border-[#f9f9fa] rounded-3xl drop-shadow-2xl">
+            <div
+              style={{
+                backgroundImage: "url(assets/images/tiger-on-bench.svg)",
+                
+              }}
+              className="mx-6 min-h-[98%]"
+            >
+              <div className="relative top-14 left-16">
+                <h1 className="mb-6 text-4xl whitespace-nowrap">Exam Portal</h1>
+                <button className="px-10 py-3 bg-white text-[#6072FA] rounded-3xl border-4 border-[#e3e3e6] w-[180px]">
+                  Give Exam
+                </button>
+              </div>
+            </div>
+            {/* <div>
+                <img src="assets/images/tiger-on-bench.svg" alt="" />
+              </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
