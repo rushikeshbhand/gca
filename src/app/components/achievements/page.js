18ee67c1924d3f776af3327ea379a33e5d0cceb3
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-10-06 13:53:50 +0530

    replace svg in place of png and also fix achievements bugs of responsiveness

--- src/app/components/achievements/page.js
+++ src/app/components/achievements/page.js
@@ -1,43 +1,49 @@
 import React from "react";
-
 export default function Achievements() {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 my-14 mx-8">
-      <div className="flex justify-center">
+    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-5 text-[#4F4D74] text-sm whitespace-nowrap justify-start lg:gap-2 my-14 mx-8" style={{fontWeight: ""}}>
+      <div className="flex justify-center items-center gap-3">
         <img
-          className="w-[13rem] lg:w-[29rem] h-auto"
+          className="w-[4rem] sm:w-[6rem] lg:w-[7rem] h-auto"
           src="assets/images/just-dial.svg"
           alt="Girl in a jacket"
           width="500"
           height="600"
         />
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">5 Stars on Just dial with 
+        <br/> 200+ User reviews</p>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center gap-3 lg:ms-16">
         <img
-         className="w-[11rem] lg:w-[15rem] h-auto"
+         className="w-[2.5rem] lg:w-[3rem] h-auto"
           src="assets/images/abp.svg"
           alt="Girl in a jacket"
           width="500"
           height="600"
         />
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">Abhinav Thakur on ABP
+        <br/> Maza Interview</p>
       </div>
-      <div className="flex justify-center"> 
+      <div className="flex justify-center items-center gap-3"> 
         <img
-         className="w-[8rem] lg:w-[11rem] h-auto"
+         className="w-[2.5rem] lg:w-[3rem] h-auto"
           src="assets/images/mced.svg"
           alt="Girl in a jacket"
           width="500"
           height="600"
         />
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">MCED Certified 
+        <br/> Company</p>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center gap-3">
         <img
-         className="w-[8rem] lg:w-[12rem] h-auto"
+         className="w-[3rem] lg:w-[4rem] h-auto"
           src="assets/images/you-tube.svg"
           alt="Girl in a jacket"
           width="500"
           height="600"
         />
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">10k+ YouTube Views</p>
       </div>
     </div>
   );
