20ac5a4e613244d29ec90801cb8e32c69d42c121
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-10-08 21:01:31 +0530

    make achievement section more responsive

--- src/app/components/achievements/page.js
+++ src/app/components/achievements/page.js
@@ -1,49 +1,67 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Achievements() {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-5 text-[#4F4D74] text-sm whitespace-nowrap justify-start lg:gap-2 my-14 mx-8" style={{fontWeight: ""}}>
+    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-5 text-[#4F4D74] text-sm whitespace-nowrap justify-start lg:gap-2 my-14 mx-8">
+      {/* Just Dial Achievement */}
       <div className="flex justify-center items-center gap-3">
-        <img
+        <Image
           className="w-[4rem] sm:w-[6rem] lg:w-[7rem] h-auto"
-          src="assets/images/just-dial.svg"
-          alt="Girl in a jacket"
-          width="500"
-          height="600"
+          src="/assets/images/just-dial.svg"
+          alt="5 Stars on Just Dial"
+          width={500}
+          height={600}
         />
-        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">5 Stars on Just dial with 
-        <br/> 200+ User reviews</p>
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">
+          5 Stars on Just dial with
+          <br /> 200+ User reviews
+        </p>
       </div>
+
+      {/* ABP Maza Interview */}
       <div className="flex justify-center items-center gap-3 lg:ms-16">
-        <img
-         className="w-[2.5rem] lg:w-[3rem] h-auto"
-          src="assets/images/abp.svg"
-          alt="Girl in a jacket"
-          width="500"
-          height="600"
+        <Image
+          className="w-[2.5rem] lg:w-[3rem] h-auto"
+          src="/assets/images/abp.svg"
+          alt="Abhinav Thakur on ABP Maza Interview"
+          width={500}
+          height={600}
         />
-        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">Abhinav Thakur on ABP
-        <br/> Maza Interview</p>
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">
+          Abhinav Thakur on ABP
+          <br /> Maza Interview
+        </p>
       </div>
-      <div className="flex justify-center items-center gap-3"> 
-        <img
-         className="w-[2.5rem] lg:w-[3rem] h-auto"
-          src="assets/images/mced.svg"
-          alt="Girl in a jacket"
-          width="500"
-          height="600"
-        />
-        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">MCED Certified 
-        <br/> Company</p>
-      </div>
+
+      {/* MCED Certification */}
       <div className="flex justify-center items-center gap-3">
-        <img
-         className="w-[3rem] lg:w-[4rem] h-auto"
-          src="assets/images/you-tube.svg"
-          alt="Girl in a jacket"
-          width="500"
-          height="600"
+        <Image
+          className="w-[2.5rem] lg:w-[3rem] h-auto"
+          src="/assets/images/mced.svg"
+          alt="MCED Certified Company"
+          width={500}
+          height={600}
         />
-        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">10k+ YouTube Views</p>
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">
+          MCED Certified
+          <br /> Company
+        </p>
+      </div>
+
+      {/* YouTube Views Achievement */}
+      <div className="flex justify-center items-center gap-3">
+        <Image
+          className="w-[3rem] lg:w-[4rem] h-auto"
+          src="/assets/images/you-tube.svg"
+          alt="10k+ YouTube Views"
+          width={500}
+          height={600}
+        />
+        <p className="text-xs sm:text-sm whitespace-normal lg:whitespace-nowrap">
+          10k+ YouTube Views
+        </p>
       </div>
     </div>
   );
