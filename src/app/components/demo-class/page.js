23c3e6d738168be7411912c0a01c673464a232a9
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-10-20 20:50:21 +0530

    add px for heading of demo class

--- src/app/components/demo-class/page.js
+++ src/app/components/demo-class/page.js
@@ -5,7 +5,7 @@ export default function DemoClass() {
   return (
     <div className='flex justify-center flex-col items-center'>
         <div className='text-center'>
-            <h1 className='text-4xl lg:text-5xl text-[#4F4D74]'>What are you looking for!</h1>
+            <h1 className='text-4xl lg:text-5xl text-[#4F4D74] px-6'>What are you looking for!</h1>
         </div>
         <div className='flex flex-row items-center justify-between bg-[#EDD93C] max-w-[85vw] sm:max-w-[90vw] sm:min-w-[90vw] lg:min-w-[65vw] lg:max-w-[65vw] px-4 py-4 sm:px-12 sm:py-8 mt-6 rounded-3xl '> 
             <div className='w-[16rem]'>
