aaaf414d9a87d8296eb640d11857aaebc8fee525
rushikeshbhand <rushikeshgbhand@gmail.com>
2024-10-25 21:00:32 +0530

    adjust demo class size

--- src/app/components/demo-class-for-landing-page/page.js
+++ src/app/components/demo-class-for-landing-page/page.js
@@ -8,14 +8,14 @@ export default function DemoClassForLandingPage() {
             <h1 className='text-4xl lg:text-5xl text-[#4F4D74] px-6'>What are you looking for!</h1>
         </div> */}
         <div className='flex flex-row items-center justify-between bg-[#EDD93C] max-w-[85vw] sm:max-w-[90vw] sm:min-w-[90vw] lg:min-w-[65vw] lg:max-w-[65vw] px-4 py-4 sm:px-12 sm:py-8 mt-6 rounded-3xl '> 
-            <div className='w-[16rem]'>
+            <div className='w-[20rem]'>
                 <Image
                 src="/assets/images/say-hello.svg"
                 width={500}
                 height={500}
                 alt="Picture of the author"></Image>
             </div>
-            <button className='w-[15rem]'>
+            <button className='w-[17rem]'>
             <Image
                 src="/assets/images/book-free-demo-class.svg"
                 width={500}
--- src/app/components/demo-class/page.js
+++ src/app/components/demo-class/page.js
@@ -5,6 +5,7 @@ export default function DemoClass() {
   return (
     <div className='flex justify-center flex-col items-center mt-6'>
         <div className='text-center'>
+            <p className='text-xl text-[#A7B0B8]'>Contact Us</p>
             <h1 className='text-4xl lg:text-5xl text-[#4F4D74] px-6'>What are you looking for!</h1>
         </div>
         <div className='flex flex-row items-center justify-between bg-[#EDD93C] max-w-[85vw] sm:max-w-[90vw] sm:min-w-[90vw] lg:min-w-[65vw] lg:max-w-[65vw] px-4 py-4 sm:px-12 sm:py-8 mt-6 rounded-3xl '> 
